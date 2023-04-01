33e2f066c944656bfb3c18644226e7009c88e1b4
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-04-01 17:52:43 +0400

    Add functionality to ask user his name and printing message in console with greetings

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const askUserName = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    return userName;
+};
+
+const greetingUser = () => {
+    console.log(`Hello, ${askUserName()}!`);
+};
+
+export { greetingUser };
