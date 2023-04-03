a0070af5702f2a0095961c441f2b1995c1099fc1
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-04-03 15:07:07 +0400

    Refactor index.js functionality brain-even.js functionality

---
+++ src/games/even.js
@@ -0,0 +1,59 @@
+import {
+  welcomeMessage,
+  getUserName,
+  greetingUser,
+  getRandomNumber,
+  getUserAnswer,
+  checkUserAnswer,
+  counterOfIterations,
+} from '../index.js';
+
+const gameRules = 'Answer "yes" if number is even, otherwise answer "no"';
+
+// Проверка на четность
+const isEven = (number) => {
+  if (number % 2 === 0) return true;
+  return false;
+};
+
+// Присваивание верного ответа для дальнейшего сравнивания с ответом пользователя
+const getCorrectAnswer = (randomNumber) => {
+  if (isEven(randomNumber)) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+// Отдельная функция на верность формата ввода ответа пользователя
+const checkCorrectnessOfUserAnswer = (userAnswer, userName) => {
+  if ((userAnswer !== 'yes') && (userAnswer !== 'no')) {
+    console.log('Not correct answer! You must answer "yes" or "no"!');
+    console.log(`Let's try again, ${userName}`);
+    return true;
+  }
+  return false;
+};
+
+// Сама игра
+const startGame = () => {
+  console.log(welcomeMessage);
+  const userName = getUserName();
+  greetingUser(userName);
+  console.log(gameRules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumber();
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = getUserAnswer();
+    if (checkCorrectnessOfUserAnswer(userAnswer, userName)) {
+      break;
+    }
+    const correctAnswer = getCorrectAnswer(randomNumber);
+    if (!checkUserAnswer(userAnswer, correctAnswer, userName)) {
+      break;
+    }
+    counterOfIterations(i, userName);
+  }
+};
+
+export default startGame;
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import readlineSync from 'readline-sync';
+
+const welcomeMessage = 'Welcome to the Brain Games!';
+
+// Функция спрашивает и возвращает имя пользователя
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
+};
+
+// Функция приветствует пользователя по имени
+const greetingUser = (userName) => {
+  console.log(`Hello, ${userName}!`);
+};
+
+// Функция возвращает случайное число от 0 до 100;
+const getRandomNumber = () => {
+  const number = Math.floor(Math.random() * 100);
+  return number;
+};
+
+// Функция запроса ответа от пользователя
+const getUserAnswer = () => {
+  const userAnswer = readlineSync.question('You answer: ');
+  return userAnswer;
+};
+
+// Функция проверки ответа пользователя и реакции на его ответ
+const checkUserAnswer = (userAnswer, correctAnswer, userName) => {
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+  console.log(`Let's try again, ${userName}`);
+  return false;
+};
+
+// Функция счётчик с поздравлениями
+const counterOfIterations = (iteration, userName) => {
+  if (iteration === 2) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export {
+  welcomeMessage,
+  getUserName,
+  greetingUser,
+  getRandomNumber,
+  getUserAnswer,
+  checkUserAnswer,
+  counterOfIterations,
+};
