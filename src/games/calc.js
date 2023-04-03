e481c44a4e6615a7d90b73e79725dde2aabea26d
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-04-03 22:00:07 +0400

    Feat calculation game in project

---
+++ src/games/calc.js
@@ -0,0 +1,55 @@
+import {
+  welcomeMessage,
+  getUserName,
+  greetingUser,
+  getRandomNumber,
+  getUserAnswer,
+  checkUserAnswer,
+  counterOfIterations,
+} from '../index.js';
+
+const gameRules = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+
+// Функция которые счиатет выражение
+const getResultOfExpression = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+// Функция c выражением которое показыввется пользователю
+const showExpression = (number1, number2, operator) => {
+  const result = `${number1} ${operator} ${number2}`;
+  return result;
+};
+
+// Сама игра
+const startGame = () => {
+  console.log(welcomeMessage);
+  const userName = getUserName();
+  greetingUser(userName);
+  console.log(gameRules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber1 = getRandomNumber(0, 100);
+    const randomNumber2 = getRandomNumber(0, 100);
+    const operator = operators[getRandomNumber(0, operators.length - 1)];
+    console.log(`Question: ${showExpression(randomNumber1, randomNumber2, operator)}`);
+    const userAnswer = Number(getUserAnswer());
+    const correctAnswer = getResultOfExpression(randomNumber1, randomNumber2, operator);
+    if (!checkUserAnswer(userAnswer, correctAnswer, userName)) {
+      break;
+    }
+    counterOfIterations(i, userName);
+  }
+};
+
+export default startGame;
