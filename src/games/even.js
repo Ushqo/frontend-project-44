c6aa0eef808b29347472a9a00b6cf432f0f6cbf7
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-04-07 17:03:15 +0400

    Refactor even.js

--- src/games/even.js
+++ src/games/even.js
@@ -5,20 +5,13 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = (randomNumber) => {
-  if (isEven(randomNumber)) {
-    return 'yes';
-  }
-  return 'no';
-};
-
-const getGameData = () => {
+function getGameData() {
   const randomNumber = getRandomNumber(0, 100);
   const gameQuestion = `${randomNumber}`;
-  const correctAnswer = getCorrectAnswer(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   const gameData = { gameQuestion, correctAnswer };
   return gameData;
-};
+}
 
 const startBrainEven = () => startGame(gameRules, getGameData);
 
